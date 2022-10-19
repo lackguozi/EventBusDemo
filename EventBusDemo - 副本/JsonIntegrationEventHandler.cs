92213c643c055b6085d18595d40d2bf68f7b4f65
lucktian <hyh01010@126.com>
2022-10-19 14:44:44 +0800

    数据修复

---
+++ EventBusDemo - 副本/JsonIntegrationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventBudDemo
+{
+    public abstract class JsonIntegrationEventHandler<T> : IIntegrationEventHandle
+    {
+        public Task Handle(string eventName, string json)
+        {
+            T? eventData = JsonSerializer.Deserialize<T>(json);
+            return HandleJson(eventName, eventData);
+        }
+        public abstract Task HandleJson(string eventName, T? eventData);
+    }
+}
